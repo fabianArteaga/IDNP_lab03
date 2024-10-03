91cb5e5184cea275a80c766ba73b1fd1a9d12835
fabianArteaga <carteaga@unsa.edu.pe>
2024-10-02 20:08:26 -0500

    Linea 32 con pendingIntent comentado

--- app/src/main/java/com/example/idnp_lab03/MainActivity.java
+++ app/src/main/java/com/example/idnp_lab03/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        //PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(Intent.ACTION_BATTERY_CHANGED), 0);
         registerReceiver(batteryReceiver, filter);
         Log.i("BatteryMonitor", "BroadcastReceiver registrado");
     }
